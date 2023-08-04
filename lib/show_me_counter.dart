cd1997635ac16317e77b58e052453faa0ce9ebe3
Byeong Gui Jeong <jsmile@naver.com>
2023-08-04 17:20:13 +0900

    show me counter basic UI

---
+++ lib/show_me_counter.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ShowMeCounter extends StatelessWidget {
+  const ShowMeCounter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Theme.of(context).colorScheme.primary,
+      appBar: AppBar(
+        title: const Text('Counter'),
+      ),
+      body: const Center(
+        child: Text(
+          '0',
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 52.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
